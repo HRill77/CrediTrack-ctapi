66598f372d56c8a4b81e2bb5ea8d535f380e8b70
harries <harriesrill.b@gmail.com>
2026-01-09 13:43:23 +0800

    Fixed

--- src/main/java/com/cta/creditrack/controllers/DataIngestionController.java
+++ src/main/java/com/cta/creditrack/controllers/DataIngestionController.java
@@ -26,7 +26,7 @@ public class DataIngestionController {
       private final DataIngestionService dataIngestionService;
 
     @PostMapping("/curricula")
-    public ResponseEntity<?> ingestCurriculaData(@RequestParam("file") MultipartFile file) {
+    public ResponseEntity<?> ingestCurriculaData(@RequestParam("file") MultipartFile file, @RequestParam("CourseCurricula") String CourseCurricula) {
         try {
             if (file.isEmpty()) {
                 Map<String, Object> response = new HashMap<>();
